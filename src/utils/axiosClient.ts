7040a5b5d01e0f6dee0351da8d572c0ac8cdedb5
LOGANLEEE <logan_lee@kakao.com>
2022-02-19 18:56:46 -0500

    disable err hanlding of interceptor

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,8 +1,10 @@
-import _axios from 'axios';
-import { encode, decode, encodingExists } from 'iconv-lite';
+import _axios, { AxiosError } from 'axios';
+import { decode } from 'iconv-lite';
 
 export const fetch = _axios.create({
 	responseType: 'arraybuffer',
+	// timeout: 60 * 1000 * 3,
+	// httpsAgent: new https.Agent({ keepAlive: true }),
 	headers: {
 		'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
 	},
@@ -15,7 +17,10 @@ fetch.interceptors.response.use(
 		}
 		return res;
 	},
-	(err) => {
-		Promise.reject(err);
-	},
+	// (err: AxiosError) => {
+	// 	console.error('Error name:', err.name);
+	// 	console.error('Error message:', err.message);
+	// 	console.error('isAxios Error:', err.isAxiosError);
+	// 	Promise.reject(err);
+	// },
 );
