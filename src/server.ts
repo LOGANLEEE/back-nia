5ba4ea62c9a2893a2c55e46342068fd925f43dd1
LOGANLEEE <logan_lee@kakao.com>
2022-02-19 19:11:25 -0500

    add /check , /post, /init
    apply env

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,40 @@
+import 'dotenv/config';
 import compression from 'compression';
 import express from 'express';
 import { initProcess } from 'crawler';
 import cors from 'cors';
+import { get_new_posts } from 'prisma';
 
 const app = express();
-const port = 4000;
 const isDebug = false;
 
 // server middle ware
-app.use(cors());
 app.use(compression());
+app.use(cors());
 
 // router
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get('/init', (req, res) => {
+	const cur = new Date();
+	console.log('======================= INIT PROCESS =======================');
+	console.log(`JOB Started at ${cur}`);
+	res.send('process started!');
+	initProcess({ isDebug: false });
 });
 
+app.get('/check', (req, res) => {
+	res.send('youhoo!');
+});
+
+app.get('/post', async (req, res) => {
+	const newPosts = await get_new_posts();
+	res.send(newPosts);
+	// console.log(`'/post' was called at ${new Date()}`);
+});
+
+const port = process.env.port || 4000;
 app.listen(port, () => {
-	isDebug && console.log(`NIA server is runnin on ${port}`);
+	console.log('env:', process.env.NODE_ENV);
+	console.log(`NIA server is runnin on ${port} v0.0.2`);
 });
 
 // job
