a199156a3c6c532f4b0c5b5f59def47c6ce52120
LOGANLEEE <logan_lee@kakao.com>
2022-04-11 21:07:21 -0400

    imple api and init process

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import compression from 'compression';
 import express from 'express';
 import { initProcess } from 'crawler';
 import cors from 'cors';
-import { get_new_posts } from 'prisma';
+import { get_new_posts, renew_test } from 'prisma';
 
 const app = express();
 const isDebug = false;
@@ -18,23 +18,35 @@ app.get('/init', (req, res) => {
 	console.log('======================= INIT PROCESS =======================');
 	console.log(`JOB Started at ${cur}`);
 	res.send('process started!');
+	initProcess({ isDebug: false });
+});
+
+app.get('/test-init', (req, res) => {
+	const cur = new Date();
+	console.log('======================= TEST INIT PROCESS =======================');
+	console.log(`JOB Started at ${cur}`);
 	initProcess({ isDebug: true });
+	res.send('process started!');
+	return;
 });
 
 app.get('/check', (req, res) => {
 	res.send('youhoo!');
+	return;
 });
 
 app.get('/post', async (req, res) => {
 	const newPosts = await get_new_posts();
 	res.send(newPosts);
+	return;
 	// console.log(`'/post' was called at ${new Date()}`);
 });
 
 const port = process.env.port || 4000;
 app.listen(port, () => {
 	console.log('env:', process.env.NODE_ENV);
-	console.log(`NIA server is runnin on ${port} v0.0.2`);
+	console.log(`NIA server is runnin on ${port} v0.0.3`);
+	return;
 });
 
 // job
@@ -42,9 +54,9 @@ app.listen(port, () => {
 setInterval(() => {
 	const cur = new Date();
 	if (cur.getMinutes() % 15 === 0) {
-		console.log('======================= INIT PROCESS =======================');
+		console.log('======================= INIT PROCESS START =======================');
 		console.log(`JOB Started at ${cur}`);
 		initProcess({ isDebug: false });
-		console.log('======================= INIT PROCESS =======================');
+		console.log('======================= INIT PROCESS DONE =======================');
 	}
 }, 59999);
