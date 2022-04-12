1952673f712f6e16edff152811b3b0485fcef235
LOGANLEEE <logan_lee@kakao.com>
2022-04-11 21:07:48 -0400

    imple set_target_from_as_old

--- src/crawler/index.ts
+++ src/crawler/index.ts
@@ -2,7 +2,7 @@ import { Prisma } from '@prisma/client';
 import * as cheerio from 'cheerio';
 import { pathInfo, siteInfo } from 'crawler/siteInfo';
 import { writeFileSync } from 'fs';
-import { clean_up_old_post, create_new_post, setNewPostAsOld } from 'prisma';
+import { clean_up_old_post, create_new_post, set_target_from_as_old } from 'prisma';
 import { fetch } from 'utils/axiosClient';
 
 interface LinkHolder {
@@ -60,12 +60,16 @@ export const first_process = ({ isDebug }: InitType) =>
 						writeFileSync(`./src/debug/${new Date()}.jsonc`, JSON.stringify(data));
 					}
 				}
+
 				// console.log('linkHolder:', linkHolder);
 				const { parsing_done, count } = await detail_parser(linkHolder, name, isDebug);
 				parsing_done ? success_count++ : fail_count++;
 				post_count += count;
 				// console.log('success_count', success_count);
 				// console.log('fail_count', fail_count);
+
+				console.log(name, siteInfo.length, success_count, fail_count, siteInfo.length === success_count + fail_count);
+
 				if (isDebug && success_count + fail_count === 1) resolve({ doProceed: true, success_count, fail_count, post_count });
 
 				if (!isDebug && siteInfo.length === success_count + fail_count) resolve({ doProceed: true, success_count, fail_count, post_count });
@@ -75,90 +79,96 @@ export const first_process = ({ isDebug }: InitType) =>
 
 const detail_parser = (linkHolder: LinkHolder[], name: string, isDebug: boolean) =>
 	new Promise<{ parsing_done: boolean; count: number }>((resolve) => {
-		const { _author, _from, _hit, _title, _upload_date, _content } = pathInfo.find(({ _from }) => _from === name);
+		try {
+			const { _author, _from, _hit, _title, _upload_date, _content } = pathInfo.find(({ _from }) => _from === name);
 
-		const length = linkHolder.length;
+			const length = linkHolder.length;
+			let count = 0;
 
-		if (_from !== undefined) {
-			const holder: Prisma.new_postsCreateManyInput[] = [];
+			if (_from !== undefined) {
+				const holder: Prisma.new_postsCreateManyInput[] = [];
 
-			linkHolder.forEach(({ href, preHit, preTitle, preAuthor, preDate }, idx) => {
-				setTimeout(async () => {
-					const { data, isError } = await fetch
-						.get(href)
-						.then((e) => ({ ...e, isError: false }))
-						.catch(() => ({ data: [], status: 999, isError: true }));
+				if (linkHolder?.length === 0) throw false;
 
-					if (!isError) {
-						const $ = cheerio.load(data);
+				linkHolder.forEach(({ href, preHit, preTitle, preAuthor, preDate }, idx) => {
+					setTimeout(async () => {
+						const { data, isError } = await fetch
+							.get(href)
+							.then((e) => ({ ...e, isError: false }))
+							.catch(() => ({ data: [], status: 999, isError: true }));
+						count++;
+						if (!isError) {
+							const $ = cheerio.load(data);
 
-						let title = undefined;
-						if (preTitle) title = preTitle;
-						else title = _title?.modifier ? _title?.modifier($(_title?.path).text()) : $(_title?.path).text();
+							let title = undefined;
+							if (preTitle) title = preTitle;
+							else title = _title?.modifier ? _title?.modifier($(_title?.path).text()) : $(_title?.path).text();
 
-						let author = undefined;
-						if (preAuthor) author = preAuthor;
-						else author = _author?.modifier ? _author?.modifier($(_author?.path).text()) : $(_author?.path).text();
+							let author = undefined;
+							if (preAuthor) author = preAuthor;
+							else author = _author?.modifier ? _author?.modifier($(_author?.path).text()) : $(_author?.path).text();
 
-						const hit = preHit || _hit?.modifier($(_hit?.path).text());
+							const hit = preHit || _hit?.modifier($(_hit?.path).text());
 
-						let upload_date = undefined;
+							let upload_date = undefined;
 
-						if (preDate) {
-							upload_date = preDate;
-						} else {
-							_upload_date?.paths.some((path) => {
-								const parsedDate = _upload_date.modifier($(path).text());
-								if (parsedDate + '' !== 'Invalid Date') {
-									upload_date = parsedDate;
-									return true;
-								}
-							});
+							if (preDate) {
+								upload_date = preDate;
+							} else {
+								_upload_date?.paths.some((path) => {
+									const parsedDate = _upload_date.modifier($(path).text());
+									if (parsedDate + '' !== 'Invalid Date') {
+										upload_date = parsedDate;
+										return true;
+									}
+								});
+							}
+
+							const content = _content?.modifier ? _content?.modifier($(_content?.path).text()) : $(_content?.path).text();
+
+							if (title !== '') {
+								holder.push({
+									title: title.trim(),
+									author: author.trim(),
+									hit,
+									upload_date,
+									content: content.replaceAll('\t', '').replaceAll('\n', ''),
+									link: href,
+									from: _from,
+									isnew: true,
+								});
+							}
 						}
-
-						const content = _content?.modifier ? _content?.modifier($(_content?.path).text()) : $(_content?.path).text();
-
-						if (title !== '') {
-							holder.push({
-								title: title.trim(),
-								author: author.trim(),
-								hit,
-								upload_date,
-								content: content.replaceAll('\t', '').replaceAll('\n', ''),
-								link: href,
-								from: _from,
-								isnew: true,
-							});
+						if (count + 1 === length) {
+							const { count: old_count, doProceed } = await set_target_from_as_old(_from);
+							console.log(`${old_count} are set old from ${_from} `);
+							if (doProceed) {
+								const { count } = await create_new_post(holder);
+								console.log(`Parser: ${_from}, ${count} posts are inserted`);
+								resolve({ parsing_done: true, count });
+							}
 						}
-					}
-
-					if (idx + 1 === length) {
-						const { count } = await create_new_post(holder);
-						console.log(`Parser: ${_from}, ${count} posts are inserted`);
-						resolve({ parsing_done: true, count });
-					}
-					// if (isDebug) throw loopBreakException;
-				}, 300 * idx);
-			});
-		} else {
+						// if (isDebug) throw loopBreakException;
+					}, 300 * idx);
+				});
+			} else {
+				throw false;
+			}
+		} catch (error) {
+			console.error('detail_parser:', error);
 			resolve({ parsing_done: false, count: 0 });
 		}
 	});
 
 export const initProcess = async ({ isDebug }: InitType) => {
-	const { doProceed, count: old_count } = await setNewPostAsOld();
+	const { doProceed: do_proceed_second, fail_count, success_count, post_count } = await first_process({ isDebug });
 
-	if (doProceed) {
-		console.log(`1: existed posts ${old_count} are set marked. `);
-		const { doProceed: do_proceed_second, fail_count, success_count, post_count } = await first_process({ isDebug });
-
-		if (do_proceed_second) {
-			console.log(`2: crawling is done. succuess:${success_count} fail:${fail_count} total post:${post_count}`);
-			const { delete_count, doProceed: do_proceed_thrid } = await clean_up_old_post();
-			if (do_proceed_thrid) {
-				console.log(`3: old posts ${delete_count} are moved `);
-				console.log('============ process done ============');
-			}
+	if (do_proceed_second) {
+		console.log(`2: crawling is done. succuess:${success_count} fail:${fail_count} total post:${post_count}`);
+		const { delete_count, doProceed: do_proceed_thrid } = await clean_up_old_post();
+		if (do_proceed_thrid) {
+			console.log(`3: old posts ${delete_count} are moved `);
+			console.log('============ process done ============');
 		}
 	}
 };
